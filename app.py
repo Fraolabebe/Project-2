e0515277939613cb331542d4788f9fe69bf53a4d
Fraol Abebe <fraolabebe@Fraols-Air.fios-router.home>
2021-02-06 15:37:54 -0500

    pushing project 2

---
+++ app.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import json
+from flask import Flask, render_template
+from sqlalchemy import create_engine
+import pymysql
+from config import remote_db_endpoint, remote_db_port, remote_db_name, remote_db_user, remote_db_pwd
+
+# set up MySQL and connect
+pymysql.install_as_MySQLdb()
+engine = create_engine(f"mysql://{remote_db_user}:{remote_db_pwd}@{remote_db_endpoint}:{remote_db_port}/{remote_db_name}")
+
+app = Flask(__name__)
+
+# Function to convert a CSV to JSON
+# Takes the file paths as arguments
+# def make_json('Datasets/employee_attrition_train.csv', jsonFilePath):
+
+@app.route("/")
+def index():
+
+    # use render_template to serve up the index.html
+
+    return render_template("index2.html")
+
+@app.route("/EmployeeData")
+def Employee_Data():
+
+    conn = engine.connect()
+
+    # Opening csv data file 
+    employee_survey_df = pd.read_sql('SELECT * FROM employee_survey', con=conn)
+    employee_survey_json = employee_survey_df.to_json(orient='records')
+   
+  #Return json to client
+    return employee_survey_json
+
+#close db connection
+    conn.close()
+
+#run the app in debug mode
+if __name__ == "__main__":
+    app.run(debug=True)
